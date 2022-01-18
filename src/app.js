bc65e2c52b953cf4d3eb78ff22dc0c7eeea739ba
BernatLl <blloret@hotmail.com>
2022-01-18 11:24:12 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// * Exercices 2
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const dataset1 = [1, 10, 20, 14, 1450, 10, 24, 12, 11, 9];
+//ejercicio 1
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const dataset2 = [
+  ["papa", "mama", "perro"],
+  [[["gato"]], "social"],
+  ["envidia"],
+  [[[[["gato"]]]]]
+];
+const matrix1 = [
+  [1, 10, 10],
+  [2, 10, 20],
+  [3, 3, 4]
+];
+const matrix2 = [
+  [1, 20, 11],
+  [9, 8, 7],
+  [1, 1, 1, 3, 4]
+];
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+const examplePoint1 = new Point(1, 2);
+const examplePoint2 = new Point(9, 7);
+
+/*
+ 1. Crea una funcion que ordene de > o < el dataset 1.
+ 2. Crea una funcion que cuente las letras en el dataset 2 (Debes usar un objeto)
+ 3. Crea una funcion que calcule la suma de todos los elementos del dataset1
+ 4. Crea una funcion que compruebe si una matriz es cuadrrada (m*n) siendo m = n (Usa matrix 1 y 2 para testear)
+ 5. Crea una funcion que calcule la distancia entre dos puntos definidos por Point2
+*/
