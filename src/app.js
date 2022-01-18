839862733689bc2f8c1ae62e916fb99812d23617
BernatLl <blloret@hotmail.com>
2022-01-18 16:31:47 +0000

    update

--- src/app.js
+++ src/app.js
@@ -2,13 +2,43 @@
 
 const dataset1 = [1, 10, 20, 14, 1450, 10, 24, 12, 11, 9];
 //ejercicio 1
-
+function Ordenar(myArr) {
+  for (let a = 0; a <= myArr.length - 1; a++) {
+    for (let b = 0; b <= myArr.length - 2; b++) {
+      if (myArr[b] > myArr[b + 1]) {
+        temp = myArr[b];
+        myArr[b] = myArr[b + 1];
+        myArr[b + 1] = temp;
+      }
+    }
+  }
+  return myArr;
+}
+console.log(Ordenar(dataset1));
+//Ejercicio 2
 const dataset2 = [
   ["papa", "mama", "perro"],
   [[["gato"]], "social"],
   ["envidia"],
   [[[[["gato"]]]]]
 ];
+//Convierto el array en un array de strings
+const dataset2join = dataset2.join("").split(",");
+//Función contar las letras del array.
+function contarLetras(letras) {
+  var objeto = {};
+  for (var i in letras) {
+    for (var j in letras[i]) {
+      objeto[letras[i][j]] = (objeto[letras[i][j]] || 0) + 1;
+    }
+  }
+  return objeto;
+}
+console.log(contarLetras(dataset2join));
+//Ejercicio 3
+const suma = array => array.reduce((a, b) => a + b, 0);
+console.log(suma(dataset1));
+//Ejercicio 4
 const matrix1 = [
   [1, 10, 10],
   [2, 10, 20],
@@ -19,6 +49,20 @@ const matrix2 = [
   [9, 8, 7],
   [1, 1, 1, 3, 4]
 ];
+const compMatrix = matrix => {
+  let x = matrix.length;
+  for (var i in matrix) {
+  }
+  let y = matrix[i].length;
+  if (y === x) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(compMatrix(matrix1));
+console.log(compMatrix(matrix2));
+//Ejercicio 5
 
 function Point(x, y) {
   this.x = x;
